aea50a237917cd0da49d7ab200f6ee647f73c4ea
Andrew Chun <crayonbox@pinterest.com>
2017-09-29 16:05:43 -0700

    Adding bottom insets to input area for iphone x

--- Source/SLKTextViewController.m
+++ Source/SLKTextViewController.m
@@ -237,6 +237,12 @@ CGFloat const SLKAutoCompletionViewDefaultHeight = 140.0;
     [super viewDidLayoutSubviews];
 }
 
+- (void)viewSafeAreaInsetsDidChange
+{
+    [super viewSafeAreaInsetsDidChange];
+
+    [self slk_updateViewConstraints];
+}
 
 #pragma mark - Getters
 
@@ -426,6 +432,10 @@ CGFloat const SLKAutoCompletionViewDefaultHeight = 140.0;
         }
     }
     
+    if (@available(iOS 11.0, *)) {
+        return self.view.safeAreaInsets.bottom;
+    }
+
     return 0.0;
 }
 
